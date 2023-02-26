f18ec9a739abf02efad24d05676af20a9ee85b67
Sharif Aly <sharif.aly_@outlook.com>
2023-02-26 23:38:59 +0100

    include start and stop function

--- src/app.js
+++ src/app.js
@@ -1,13 +1,56 @@
-const seconds = document.getElementById("seconds");
-const minutes = document.getElementById("minutes");
-let second = 0;
-let minute = 0;
-const btnStart = document.getElementById("btn-start");
-const btnReset = document.getElementById("btn-reset");
-const btnStop = document.getElementById("btn-Stop");
+// const seconds = document.getElementById("seconds");
+// const minutes = document.getElementById("minutes");
+// let second = 0;
+// let minute = 0;
+// const btnStart = document.getElementById("btn-start");
+// const btnReset = document.getElementById("btn-reset");
+// const btnStop = document.getElementById("btn-Stop");
 
-btnStart.addEventListener("click", () => {
-  let interval = setInterval(function () {
-    seconds.innerHTML = second++;
-  }, 1000);
-});
+// btnStart.addEventListener("click", () => {
+//   let interval = setInterval(function () {
+//     seconds.innerHTML = second++;
+//   }, 1000);
+//   btnReset.addEventListener;
+// });
+
+// btnStop.addEventListener("click", () => {
+//   return clearInterval(interval);
+// });
+
+let [seconds, minutes, hours] = [0, 0, 0];
+let displayTime = document.getElementById("display-time");
+let timer = null;
+
+function stopwatch() {
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+
+function watchStart() {
+  if (timer !== null) {
+    clearInterval(timer);
+  }
+  (timer = setInterval(stopwatch)), 1000;
+}
+
+function watchStop() {
+  clearInterval(timer);
+}
+
+function watchReset() {
+  clearInterval(timer);
+  displayTime.innerHTML = "00:00:00";
+}
